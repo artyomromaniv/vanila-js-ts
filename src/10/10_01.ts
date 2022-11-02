23a2d1e3dd5041b78bceeac973bb4986653311aa
artyomromaniv <romaniv.artyom8@gmail.com>
2022-11-03 02:44:15 +0300

    upgradeLaptop

---
+++ src/10/10_01.ts
@@ -0,0 +1,30 @@
+export type UserType = {
+    name: string
+    hair: number
+    address: {
+        city: string,
+        house?: number
+    }
+}
+export type LaptopType = {
+    title: string
+}
+export type UserWithLaptopType = UserType & {
+    laptop: LaptopType
+}
+
+export function makeHairstyle(u: UserType, power: number) {
+    const copy = {
+        ...u,
+        hair: u.hair / power
+    }
+    return copy
+}
+
+export function moveUser(u: UserWithLaptopType, city: string) {
+    return {...u, address: {...u.address, city: city}}
+}
+
+export function upgradeUserLaptop(u:UserWithLaptopType,title:string){
+    return {...u,laptop: {...u.laptop, title:title}}
+}
